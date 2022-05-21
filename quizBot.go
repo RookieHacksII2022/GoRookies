9d515323fca2751ca8118de9681f122a20b8034e
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2022-05-21 22:12:27 +0800

    Implement start function

--- quizBot.go
+++ quizBot.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
+	firebase "firebase.google.com/go"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var optionKeyboard = tgbotapi.NewReplyKeyboard(
@@ -16,11 +23,22 @@ var optionKeyboard = tgbotapi.NewReplyKeyboard(
 	),
 )
 
-// func sendMsg() {
-
-// }
-
 func main() {
+
+	// init firebase client
+	opt := option.WithCredentialsFile("firebase_service_acct.json")
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		fmt.Errorf("error initializing app: %v", err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer client.Close()
 
 	// Load the telegram bot key
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
@@ -43,7 +61,8 @@ func main() {
 	// inactive -> current user has not be logged by bot. User is otherwise logged by the bot
 	// idle -> user has been logged by bot and waiting command
 
-	var currentUser string = ""
+	var currentUsername string = ""
+	var currentUserID string = ""
 
 	for update := range updates {
 		// ignore non-Message updates
@@ -58,11 +77,52 @@ func main() {
 
 			if update.Message.Command() == "start" {
 				// Check if the focus user id is already in the USERS collection, else create new user
-				currentUser = update.Message.From.UserName
+				currentUsername = update.Message.From.UserName
+				currentUserID = fmt.Sprint(update.Message.From.ID)
+
+				docRef := client.Collection("USERS").Doc(currentUserID)
+				doc, err := docRef.Get(ctx)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						// // Handle document not existing here
+						// _, err := docRef.Set(ctx /* custom object here */)
+						// if err != nil {
+						// 	return err
+						// }
+					} else {
+						// return err
+					}
+				}
+
+				if doc.Exists() {
+					// Handle document existing here
+					fmt.Println("User found")
+
+				} else {
+					// Create new user document
+					_, err := client.Collection("USERS").Doc(currentUserID).Set(ctx, map[string]interface{}{
+						"username": currentUsername,
+					})
+
+					if err != nil {
+						log.Fatalf("Failed adding [%s]: %v", currentUsername, err)
+					}
+
+					// Create new user quizzes collection
+					_, err2 := client.Collection("USERS").Doc(currentUserID).Collection("QUIZZES").Doc("demo quiz").Set(ctx, map[string]interface{}{
+						"numQns":                       "1",
+						"score":                        "none",
+						"this is a demo quiz question": "this is a demo quiz answer",
+					})
+
+					if err2 != nil {
+						log.Fatalf("Failed adding quizzes collection for [%s]: %v", currentUsername, err2)
+					}
+				}
 
 				// Return message: "Hello <username>!"
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-				msg.Text = "Hello " + currentUser + "!"
+				msg.Text = "Hello " + currentUsername + "!"
 
 				if _, err := bot.Send(msg); err != nil {
 					log.Panic(err)
