5ccaa60ad2eecca6070b0601e66e39f8217c5ade
hua-lun <chuahualun@gmail.com>
2022-05-22 01:59:45 +0800

    Update test case

--- quizBot_test.go
+++ quizBot_test.go
@@ -8,7 +8,7 @@ func TestParser(t *testing.T) {
 	originalStr := "/copycat testing 123"
 	outputStr := Parser(originalStr)
 
-	if outputStr != "testing" {
+	if outputStr != "testing 123" {
 		t.Error("Expected: " + "testing but got: " + outputStr)
 	}
 }
